ad46813015c46cb3ea24c4836652b322d098247a
trimakichan <115846240+trimakichan@users.noreply.github.com>
2024-01-09 05:36:29 -0800

    ProfileImage component created

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="container">
       <Header />
-      {/* <HomeScreen /> */}
+      <HomeScreen />
       <ProfileImage />
     </div>
   );
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,33 +1,23 @@
 import "./main.scss";
-import profile from "../../assets/images/profilePic.webp";
+import ProfileImage from '../ProfileImage/ProfileImage'
 
 const Main = () => {
   return (
     <div className="main">
       <div className="main__contentWrapper">
-        <section className="main__content grid-col-span-3">
+        <section className="main__content">
           <h2>Makiko Yokoyama</h2>
-          {/* <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis eum
-            beatae itaque doloribus nisi, sunt temporibus praesentium sapiente
-            similique deleniti porro, hic, quae quod voluptates quidem aliquam
-            possimus? Ea, provident.
-          </p> */}
-        </section>
-        <section className="main__profilePic">
-          <div className="profile-imgBg">
-          <div  className="profile__picWrapper">
-            <img src={profile} className="profile__pic"/>
-          </div>
-          </div> 
-        </section>
-
-           <p>
+          <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis eum
             beatae itaque doloribus nisi, sunt temporibus praesentium sapiente
             similique deleniti porro, hic, quae quod voluptates quidem aliquam
             possimus? Ea, provident.
           </p>
+        </section>
+        <section>
+            <ProfileImage />
+        </section>
+
       </div>
     </div>
   );
