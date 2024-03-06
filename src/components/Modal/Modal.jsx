921afd6447bc45a82986fae6effd82f28c83dd9c
trimakichan <115846240+trimakichan@users.noreply.github.com>
2024-03-06 13:11:17 -0800

    Working on modal styles

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,29 +1,37 @@
-import {useContext} from 'react'
+import { useContext } from 'react'
 import './modal.scss'
 import { ModalContext } from '../../contexts/ModalContext'
 
 const Modal = () => {
-    const {setModalStatus, modalData} = useContext(ModalContext);
-  return (
-    <div className='modal modal-background'>
-        <div className='modal__container'>
-            <button onClick={() => setModalStatus(false)}> X </button>
-            <div className="modal__title">
-                <h1>{modalData.item.name}</h1>
-            </div>
+    const { setModalStatus, modalData } = useContext(ModalContext);
+    return (
+        <div className='modal modal-background'>
+            <div className='modal__container'>
+                {/* <button onClick={() => setModalStatus(false)}> X </button> */}
 
-            <div className="modal__body">
-                <p>{modalData.item.description}</p>
-            </div>
+                <div className='modal__image'>
+                    <img src={modalData.item.image}/>
+                </div>
 
-            <div className="modal__footer">
-            <button onClick={() => setModalStatus(false)}>Cancel</button>
-            <button>Link</button>
+                <div className='modal__content'>
+                    <div className="modal-title">
+                        <h1>{modalData.item.name}</h1>
+                    </div>
 
+                    <div className="modal-body">
+                        <p>{modalData.item.description}</p>
+                    </div>
+
+                    <div className="modal-footer">
+                        <button onClick={() => setModalStatus(false)}>Cancel</button>
+                        <button>Link</button>
+
+                    </div>
+
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Modal
